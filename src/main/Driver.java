ac5722a71988430410569d4cca0631ab598b34c8
ZY <140974456+axpn@users.noreply.github.com>
2024-11-06 17:33:53 +0800

    ver2

--- src/main/Driver.java
+++ src/main/Driver.java
@@ -1,7 +1,5 @@
 package main;
-import models.Containment;
-import models.Jewellery;
-import models.JewelleryStore;
+import models.*;
 import utils.ScannerInput;
 
 public class Driver {
@@ -15,21 +13,21 @@ public class Driver {
 
     private int jewelleryStore() {
         System.out.println("""
-                 --------- Jewellery Store ---------
-                 | 1) Display case MENU            |
-                 | 2) Display tray MENU            |
-                 | 3) Display jewellery MENU       |
-                 |---------------------------------|
-                 | 4) View all stock               |
-                 | 5) Search for items             |
-                 | 6) Smart add                    |
-                 | 7) Smart remove                 |
-                 | 8) Value stock                  |
-                 |---------------------------------|
-                 | 9) Reset                        |
-                 | 10) Save and Load               |
-                 |---------------------------------|
-                 | 0) Exit                         |
+                 -------- Jewellery Store --------
+                 | 1) Manage jewellery store     |
+                 | 2) Display case MENU          |
+                 | 3) Display tray MENU          |
+                 |-------------------------------|
+                 | 4) View all stock             |
+                 | 5) Search for items           |
+                 | 6) Smart add                  |
+                 | 7) Smart remove               |
+                 | 8) Value stock                |
+                 |-------------------------------|
+                 | 9) Reset                      |
+                 | 10) Save and Load             |
+                 |-------------------------------|
+                 | 0) Exit                       |
                 """);
         return ScannerInput.readNextInt("==>> ");
     }
@@ -38,9 +36,9 @@ public class Driver {
         int option = jewelleryStore();
         while (option != 0) {
             switch (option) {
-                case 1 -> runDisplayCaseMENU();
-                case 2 -> runDisplayTrayMENU();
-                case 3 -> runJewelleryMENU();
+                case 1 -> manageJewelleryStore();
+                case 2 -> runDisplayCaseMENU();
+                case 3 -> runDisplayTrayMENU();
                 case 4 -> showAllStock();
                 case 5 -> searchForItems();
                 case 6 -> smartAdd();
@@ -57,26 +55,54 @@ public class Driver {
         System.out.println("Exiting...");
     }
 
-    private int displayCaseMENU() {
+    private int manageJewelleryStore() {
         System.out.println("""
-                ---------Display case MENU ---------
-                | 1) Add a new display case        |
-                | 2) Delete a display case         |
-                | 3) List all display case         |
-                | 4) Return to Jewellery Store     |
+                --------Manage Jewellery Store--------
+                | 1) Add a new display case          |
+                | 2) Delete a display case           |
+                | 3) List all display case           |
+                | 0) Return to last menu             |
                 """);
         return ScannerInput.readNextInt("==>> ");
     }
 
-    private void runDisplayCaseMENU(){
-        int option = displayCaseMENU();
+    private void runStore(){
+        int option = manageJewelleryStore();
         while (option != 0) {
             switch (option) {
                 case 1 -> addDisplayCase();
                 case 2 -> deleteDisplayCase();
-                case 3 -> System.out.println();
+                case 3 -> System.out.println(DisplayCase);
+                case 0 -> runJewelleryStore();
+                default -> System.out.println("Invalid option entered" + option);
             }
+            ScannerInput.readNextLine("\n Press the enter key to continue");
+            option = manageJewelleryStore();
         }
     }
 
+    private int displayCaseMENU() {
+        System.out.println("""
+                --------Display Case Menu--------
+                | 1) Add a new display tray     |
+                | 2) Delete a display tray      |
+                | 3) List all display tray      |
+                | 0) Return to last menu        |
+                """);
+        return ScannerInput.readNextInt("==>> ");
+    }
+
+    private void runDisplayCaseMENU() {
+        int option = displayCaseMENU();
+        while (option != 0) {
+            switch (option) {
+                case 1 -> addDisplayTray();
+                case 2 -> deleteDisplayTray();
+                case 3 -> System.out.println(DisplayTray);
+                case 0 -> runStore();
+            }
+            ScannerInput.readNextLine("\n Press the enter key to continue");
+            option = displayCaseMENU();
+        }
+    }
 }
