9a106a1dfc9856cf8537fcad635f245133628b1b
shiqian369 <3164309607@qq.com>
2024-11-07 01:04:57 +0800

    1.2

--- src/models/JewelleryStore.java
+++ src/models/JewelleryStore.java
@@ -1,4 +1,37 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class JewelleryStore {
+    private List<DisplayCase> cases;
+
+    public JewelleryStore() {
+        this.cases = new ArrayList<>();
+    }
+
+    // 添加展示柜
+    public void addDisplayCabinet(DisplayCase cabinet) {
+        cases.add(cabinet);
+    }
+
+    // 获取所有展示柜
+    public List<DisplayCase> getCabinets() {
+        return cases;
+    }
+
+    // 清空所有数据
+    public void clearStore() {
+        cases.clear();
+    }
+
+    // 保存和加载系统数据
+    public void saveData(String filename) {
+        // TODO: 实现数据持久化保存
+    }
+
+    public void loadData(String filename) {
+        // TODO: 实现数据加载
+    }
 }
+
