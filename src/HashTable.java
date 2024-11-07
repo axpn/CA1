2d6bbe05fc7f1b3e992733084f2493d699da1283
qs50 <153702973+qs50@users.noreply.github.com>
2024-11-07 16:17:06 +0800

    加了添加链表的方法

---
+++ src/HashTable.java
@@ -0,0 +1,13 @@
+public class HashTable<E> {
+    private LinkedList<E>[] hashTable;
+    private int size;
+
+    public HashTable(int size) {
+        this.size = size;
+        hashTable = new LinkedList[size];
+        for (int i = 0; i < size; i++) {
+            hashTable[i] = new LinkedList<>();
+        }
+    }
+
+}
---
+++ src/LinkedList.java
@@ -0,0 +1,50 @@
+public class LinkedList<E> {
+    private class Node {
+        E data;
+        Node next;
+        Node prev;
+
+        public Node(E data) {
+            this.data = data;
+        }
+    }
+
+    // Fields for head, tail, and size
+    private Node head;
+    private Node tail;
+    private int size;
+
+    // Constructor
+    public LinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    public void addNode(E data) {
+        Node newNode = new Node(data);
+        if (tail == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    public void addLinkedList(LinkedList list) {
+        if (list.head == null) {
+            return;
+        }
+        if (this.tail == null) {
+            this.head = list.head;
+            this.tail = list.tail;
+        } else {
+            this.tail.next = list.head;
+            list.head.prev = this.tail;
+            this.tail = list.tail;
+        }
+    }
+}
