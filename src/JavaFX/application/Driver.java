53e9b00cf94874b503059fad1cb10224ecfb6c65
ZY <140974456+axpn@users.noreply.github.com>
2024-11-08 18:50:41 +0800

    00

--- src/JavaFX/application/Driver.java
+++ src/JavaFX/application/Driver.java
@@ -1,9 +1,19 @@
 package JavaFX.application;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class Driver extends Application{
+Button button;
+    public static void main(String[] args) {
+        launch(args);
+    }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        button =
+    }
 }
  
--- src/JavaFX/application/JSController.java
+++ src/JavaFX/application/JSController.java
@@ -1,4 +1,45 @@
 package JavaFX.application;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Alert;
+import models.DisplayCase;
+import models.JewelleryStore;
+
 public class JSController {
+
+    private JewelleryStore jewelleryStore;
+    private TextField idField;
+    private ComboBox<String>  typeComboBox;
+    private ComboBox<String>  lightingComboBox;
+    @FXML
+    private Button addNewDisplayCase;
+    private Button addNewDisplayTray;
+    private Button addNewJewelleryItem;
+    private Label statusLabel;
+
+    public JSController(){
+        jewelleryStore = new JewelleryStore();
+    }
+
+    @FXML
+    private void handleAddNewDisplayCase() {
+        String id = getID();
+        String type = typeComboBox.getValue();
+        String lighting = lightingComboBox.getValue();
+
+        if (id == null || type == null || lighting == null) {
+            showAlert
+        }
+        DisplayCase displayCase = new DisplayCase(id,type,lighting);
+    }
+
+    private String getID(){
+        return idField.getText();
+    }
 }
